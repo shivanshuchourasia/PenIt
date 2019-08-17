1c2a5eb32a9e00f12991b0c9f710861139035c14
Shivanshu Chourasia <shivansh4011@gmail.com>
2019-08-17 19:35:01 +0530

    Add no-cache header in ensureAuthenticated middleware

--- helpers/auth.js
+++ helpers/auth.js
@@ -1,6 +1,9 @@
 module.exports = {
-  ensureAuthenticated: function(req, res, next){
-    if(req.isAuthenticated()){
+  ensureAuthenticated: function (req, res, next) {
+    if (req.isAuthenticated()) {
+      res.header('Cache-Control', 'private, no-cache, no-store, must-revalidate');
+      res.header('Expires', '-1');
+      res.header('Pragma', 'no-cache');
       return next();
     }
     req.flash('error_msg', 'Not Authorised');
