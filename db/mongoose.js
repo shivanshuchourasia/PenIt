3c126cdc29dbb7350e0bf0494a947875151be28f
Shivanshu Chourasia <shivansh4011@gmail.com>
2019-08-17 17:34:40 +0530

    Minor fix

--- db/mongoose.js
+++ db/mongoose.js
@@ -8,5 +8,3 @@ if(!process.env.NODE_ENV){
 mongoose.connect(process.env.DB, {useNewUrlParser: true, useFindAndModify: false})
   .then(() => console.log('MongoDB Connected...'))
   .catch((e) => console.log(e));
-//WWzy59NMn5UtqvDe
-//mongodb+srv://Shivanshu:<password>@penit-db-f49os.mongodb.net/test?retryWrites=true&w=majority
